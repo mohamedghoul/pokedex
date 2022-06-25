9cad4408186ede0e9e379028399f3833532ece03
m-ghoul <motheghoul@gmail.com>
2022-06-25 13:18:21 +0300

    Added popup to view pokemon details
    - Added CSS code to format the popup view and the buttons
    - Added JS logic to show and close the popup as well as display

--- js/index.js
+++ js/index.js
@@ -2,49 +2,66 @@
 const pokedex = document.getElementById('pokedex')
 
 // Function to fetch the pokemon data from the PokeAPI and save them locally
-function fetchPokemon() {
-    // Putting promises in an array since resolving promises in a for loop is not recommended when we can use Promise.all
-    const promises = []
-    // Used a count of 50 due to the large number of pokemon in the database (1000+!!)
-    // Could potentially add support for pagination in the future to mitigate this issue.
-    const count = 50
-    // Create a promise for each pokemon and push it to the promises array
-    for(let i = 1; i <= count; i++){
-        let url = `https://pokeapi.co/api/v2/pokemon/${i}`
-        promises.push(fetch(url).then(response => response.json()))
-    }
-
-    // Resolve all promises and map the data we need
-    Promise.all(promises).then((results) => {
-        const pokemon = results.map((result) => ({
-            name: result.name,
-            id: result.id,
-            image: result.sprites.front_default,
-            species: result.species.name,
-            height: result.height,
-            weight: result.weight,
-            gender: (result.front_female == null) ? "male": "female", // To be dealt with later using dedicated endpoint
-            stats: result.stats,
-            moves: result.moves,
-            type: result.types.map((type) => type.type.name).join(', ')
-        }))
-        // Display the pokemon on the home screen
-        displayPokemonOnHomeScreen(pokemon)
-    })
+async function fetchPokemons() {
+    const url = 'https://pokeapi.co/api/v2/pokemon/?limit=50'
+    const response = await fetch(url)
+    const data = await response.json()
+    const pokemons = data.results.map((result, index) => ({
+        ...result,
+        name: result.name,
+        id: index + 1,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png`
+    }))
+    displayPokemonOnHomeScreen(pokemons)
 }
-
+ 
 // Function to display Pokemon on the home screen
-function displayPokemonOnHomeScreen(pokemon) {
-    const pokemonToHTML = pokemon.map((pokemon) => `
-    <li class="card">
+function displayPokemonOnHomeScreen(pokemons) {
+    // HTML string to inject into the home page
+    const pokemonToHTML = pokemons.map((pokemon) => `
+    <li class="card" onclick="selectPokemon(${pokemon.id})">
     <h4 class="card-id">${pokemon.id}</h4>
     <img class="card-image" src="${pokemon.image}"/>
     <h2 class="card-name">${pokemon.name}</h2>
-    <p class="card-type">Type: ${pokemon.type}</p>
     </li>
     `
     ).join('')
     pokedex.innerHTML = pokemonToHTML
 }
 
-fetchPokemon()
+// Function that is executed when a user clicks on a pokemon
+// Fetches the individual pokemon's data from the PokeAPI and calls another function to display the data in a popup window
+async function selectPokemon(id) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const response = await fetch(url)
+    const pokemon = await response.json()
+    displayPopup(pokemon)
+}
+
+// Function that displays pokemon details in a popup window
+function displayPopup(pokemon){
+    const type = pokemon.types.map((type) => type.type.name).join(', ')
+    const image = pokemon.sprites['front_default']
+    // HTML string for the popup window
+    const pokemonDetailsToHTML = `
+    <div class="popup">
+        <button id="close-popup" onclick="closePopup()">X</button>
+        <div class="card">
+            <img class="card-image" src="${image}"/>
+            <h2 class="card-name">${pokemon.id}. ${pokemon.name}</h2>
+            <p><small>Height: </small>${pokemon.height} | <small>Weight: </small>${pokemon.weight} | <small>Type: </small>${type}</p>
+        </div>
+    </div>
+    `
+    console.log(pokemonDetailsToHTML)
+    pokedex.innerHTML = pokemonDetailsToHTML + pokedex.innerHTML
+}
+
+// Function to close the popup and restore the home page to its original state
+function closePopup() {
+    const popup = document.querySelector(".popup")
+    popup.parentElement.removeChild(popup)
+    fetchPokemons()
+}
+
+fetchPokemons()
